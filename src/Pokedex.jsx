c8264a67b2ae5fe5ce36fa25d7fe767709d1b68f
Jose David Ramirez <117590011+jdramirezu@users.noreply.github.com>
2025-01-21 05:17:21 -0500

    Implemented Pokedex image

---
+++ src/Pokedex.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PokedexImg from './assets/Pokedex.png'
+
+const Pokedex = () =>{
+    return (
+        <img src={PokedexImg} alt="Pokedex"/>
+    );
+}
+
+export default Pokedex;
